JoonghyunCho/TestBed

Add ShellHandler (#64)
* Add ShellHandler

* Move ShellView into Platform/Tizen

* Update ShellView

* Move the code for embedded tizen resources to csproj

* Add UIExtenstions for shell

src/Controls/src/Core/Hosting/AppHostBuilderExtensions.cs
@@ -12,7 +12,7 @@ namespace Microsoft.Maui.Controls.Hosting
 	{
 		static readonly Dictionary<Type, Type> DefaultMauiControlHandlers = new Dictionary<Type, Type>
 		{
-#if WINDOWS || __ANDROID__
+#if WINDOWS || __ANDROID__ || TIZEN
 			{ typeof(Shell), typeof(ShellHandler) },
 #endif
 			{ typeof(ActivityIndicator), typeof(ActivityIndicatorHandler) },

JoonghyunCho/maui

src/Controls/src/Core/Hosting/AppHostBuilderExtensions.cs
@@ -59,7 +59,8 @@ namespace Microsoft.Maui.Controls.Hosting
 			handlersCollection.AddHandler<MenuFlyoutItem, MenuFlyoutItemHandler>();
 			handlersCollection.AddHandler<MenuBarItem, MenuBarItemHandler>();
 
-#if ANDROID || IOS
+#if ANDROID || IOS || TIZEN
+			handlersCollection.AddHandler<RefreshView, RefreshViewHandler>();
 			handlersCollection.AddHandler<SwipeItemView, SwipeItemViewHandler>();
 #endif
 #if WINDOWS || ANDROID